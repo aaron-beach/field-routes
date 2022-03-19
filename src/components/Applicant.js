939bc006162af006453bc2fff9df20c4dda20c38
aaronbeach <aaron.r.beach@gmail.com>
2022-03-18 21:21:51 -0400

    [add] Applicant section

---
+++ src/components/Applicant.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { P } from './type';
+import { COLORS } from './shared';
+
+const Applicant = () => {
+
+  const Wrapper = styled.div`
+		align-items: center;
+		display: flex;
+    flex-direction: column;
+    height:  175px;
+    justify-content: center;
+	`;
+
+  const ApplicantText = styled( P )`
+    color: ${COLORS.DARK_BLUE};
+    font-size: 32px;
+  `;
+
+  const LinkedIn = styled.img`
+    height: 40px
+  `;
+  
+  return (
+    <Wrapper>
+      <ApplicantText>Aaron Beach</ApplicantText>
+      <LinkedIn src='./LI.png'/>
+    </Wrapper>
+  )
+};
+
+export default Applicant;
