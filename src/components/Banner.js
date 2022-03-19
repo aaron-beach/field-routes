36f0cd6431b55f18e43381f305deffdaaa1cfa35
aaronbeach <aaron.r.beach@gmail.com>
2022-03-18 21:22:12 -0400

    [add] Banner section

---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { COLORS } from './shared';
+import { PageTitle } from './type';
+
+
+const Banner = () => {
+
+  const BannerText = styled(PageTitle)`
+    height: 256px;
+    line-height: 256px;
+    color: ${COLORS.WHITE};
+    margin-top: 100px;
+    background-color: ${COLORS.DARK_BLUE};
+    font-style: italic;
+    text-align: center;
+  `;
+  return (
+		<>
+			<BannerText>FieldRoutes Sales & Marketing Dev Test</BannerText>
+		</>
+	);
+};
+
+export default Banner;
